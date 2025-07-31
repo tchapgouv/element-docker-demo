07c6b897d0150ee9f45d04ac02ffd56384b7d42e
olivier <odelcroi@gmail.com>
2025-07-31 16:19:20 +0200

    add identity server proxy

--- start_tchap_light.sh
+++ start_tchap_light.sh
@@ -5,4 +5,4 @@ set -e
 cp tchap/nginx/.app.local.dev.light.conf data-template/nginx/conf.d/app.conf
 cp tchap/synapse/homeserver.local.dev.light.yaml data-template/synapse/homeserver.yaml
 
-docker compose -f compose-tchap-light.yml up
+docker compose -f compose-tchap-light.yml up -d
