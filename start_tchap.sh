0e0b21708383b2fae732161ae0418e53fbec9ff1
olivier <odelcroi@gmail.com>
2025-09-08 17:23:02 +0200

    use docker compose profiles, add mas service

--- start_tchap.sh
+++ start_tchap.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-cp tchap/nginx/.app.local.dev.without.mas.conf data-template/nginx/conf.d/app.conf
-cp tchap/synapse/homeserver.local.dev.without.mas.yaml data-template/synapse/homeserver.yaml
+docker compose down
 
-docker compose -f compose-tchap.yml up
+cp tchap/nginx/.app.local.dev.light.conf data-template/nginx/conf.d/app.conf
+cp tchap/synapse/homeserver.local.dev.light.yaml data-template/synapse/homeserver.yaml
+cp tchap/mas/config.local.dev.yaml data-template/mas/config.yaml
+
+
+export COMPOSE_PROFILES="tchap,with_mas"
+# Lancer les deux fichiers compose ensemble
+#docker compose -f compose.yml -f compose-tchap-additional-services.yml up -d
+docker compose -f compose.yml -f compose-tchap-additional-services.yml up -d
