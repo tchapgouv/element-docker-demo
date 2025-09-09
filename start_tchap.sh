58c290ea2fb37c86b377e8c682cf11f09300d3b6
olivier <odelcroi@gmail.com>
2025-09-09 10:12:16 +0200

    add profile for tchap-web

--- start_tchap.sh
+++ start_tchap.sh
@@ -9,7 +9,7 @@ cp tchap/synapse/homeserver.local.dev.light.yaml data-template/synapse/homeserve
 cp tchap/mas/config.local.dev.yaml data-template/mas/config.yaml
 cp tchap/tchap-web/config.local.json data-template/tchap-web/config.local.json
 
-export COMPOSE_PROFILES="tchap,with_mas"
+export COMPOSE_PROFILES="tchap,with_mas,with_web"
 # Lancer les deux fichiers compose ensemble
 #docker compose -f compose.yml -f compose-tchap-additional-services.yml up -d
 docker compose -f compose.yml -f compose-tchap-additional-services.yml up -d
