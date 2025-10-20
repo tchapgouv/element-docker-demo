c24b5d19ff7d834c8fba7f5e99a5132792d28627
Olivier D <odelcroi@gmail.com>
2025-10-20 18:05:49 +0200

    Fix components version (#8)
    * pull image versions at each start
    
    * buff login limit rate
    
    * buff login ratelimit
    
    * pull only tchap images
    
    * fix synapse version
    
    * Apply suggestion from @odelcroi
    
    Co-authored-by: Olivier D <odelcroi@gmail.com>
    
    * Apply suggestion from @odelcroi
    
    Co-authored-by: Olivier D <odelcroi@gmail.com>
    
    * Apply suggestion from @odelcroi
    
    Co-authored-by: Olivier D <odelcroi@gmail.com>
    
    ---------
    
    Co-authored-by: mcalinghee <mcalinghee.dev@gmail.com>

--- start_tchap.sh
+++ start_tchap.sh
@@ -10,5 +10,8 @@ cp tchap/synapse/homeserver.local.dev.light.yaml data-template/synapse/homeserve
 cp tchap/mas/config.local.dev.yaml data-template/mas/config.yaml
 cp tchap/tchap-web/config.local.json data-template/tchap-web/config.local.json
 
+# pull latest version of images of tchap-web and mas-tchap
+docker compose -f compose.yml -f compose-tchap-additional-services.yml pull tchap-web mas-tchap
+
 # start docker compose with main services and tchap services
 docker compose -f compose.yml -f compose-tchap-additional-services.yml up -d
